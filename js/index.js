3709e8b9af0bcd2bb61f4c39474aa0fbc815406f
Nikhil Sengar <nsengar@modeln.com>
2020-06-14 20:37:54 +0530

    + Prepared initial setup + added a welcome page.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).on('click', function () {
+  $('.welcome-title').slideUp(1000);
+  $('.display-on-load').show();
+});
+
+$(document).on('keypress', function () {
+  $('.welcome-title').slideUp(1000);
+  $('.display-on-load').show();
+});
